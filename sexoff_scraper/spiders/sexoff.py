85c04eba2377c5c3058f99ad0e858d8c73e6351e
Sean Stoops <brutimus@gmail.com>
2014-12-02 17:34:45 -0800

    Removed Selenium requirement.

--- sexoff_scraper/spiders/sexoff.py
+++ sexoff_scraper/spiders/sexoff.py
@@ -8,24 +8,40 @@ import scrapy
 from scrapy import log
 from scrapy.http import Request, FormRequest
 from scrapy.selector import Selector
-from scrapy.utils.response import open_in_browser
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 
 from sexoff_scraper.items import SexoffScraperItem
+
+
+ID_RE = re.compile(r'.*\(\'(\w+)\'\)$')
 
 
 class SexoffSpider(scrapy.Spider):
     name = "sexoff"
     allowed_domains = ["www.meganslaw.ca.gov"]
     start_urls = (
-        'http://www.meganslaw.ca.gov/disclaimer.aspx?lang=ENGLISH',
+        'http://www.meganslaw.ca.gov/cgi/prosoma.dll?searchby=curno',
     )
     pagination_url = "http://www.meganslaw.ca.gov/cgi/prosoma.dll?w6=%s&searchby=CountyList&SelectCounty=ORANGE&SB=0&PageNo=%s"
     profile_url = "http://www.meganslaw.ca.gov/cgi/prosoma.dll?w6=%s&searchby=offender&id=%s"
     session_id = None
     pages = None
-    id_re = re.compile(r'.*\(\'(\w+)\'\)$')
+
+    def parse_first_page(self, response):
+        sel = Selector(response)
+        self.pages = int(sel.xpath(
+            '((//td[@align="right" and @valign="top"])[1]/a)[last()]/text()'
+            ).extract()[0].strip())
+
+        # Unroll the first page's requests
+        for req in self.parse_page(response):
+            yield req
+
+        # Make the rest of the pagination requests
+        for x in xrange(1, self.pages + 1):
+            self.log('Requesting page: %s' % x, level=log.INFO)
+            yield Request(
+                self.pagination_url % (self.session_id, x),
+                callback=self.parse_page)
 
     def parse_page(self, response):
         self.log('Processing page of results', level=log.INFO)
@@ -34,10 +50,10 @@ class SexoffSpider(scrapy.Spider):
         hrefs = sel.xpath(
             '(//table[@id="table14"]//table)[1]//tr[position()>1]/td[1]/a/@href'
             ).extract()
-        ids = map(lambda x:self.id_re.findall(x)[0], hrefs)
-        self.log('Profile IDs:', str(ids))
+        ids = map(lambda x:ID_RE.findall(x)[0], hrefs)
+        self.log('Profile IDs: %s' % str(ids))
         for x in ids:
-            self.log('Requesting profile:', x, level=log.INFO)
+            self.log('Requesting profile: %s' % x, level=log.INFO)
             yield Request(
                 self.profile_url % (self.session_id, x),
                 self.parse_profile)
@@ -71,40 +87,13 @@ class SexoffSpider(scrapy.Spider):
             '//td[@headers="lastKnwnAddrColHdr"]/text()').extract()[0].strip()
         return item
 
-    def login(self):
-        self.log('Logging into the Megan\'s Law system with Selenium', level=log.INFO)
-        self.log('Loading disclaimer', level=log.INFO)
-        driver = webdriver.Chrome()
-        driver.get('http://www.meganslaw.ca.gov/disclaimer.aspx?lang=ENGLISH')
-        time.sleep(2)
-        self.log('Accepting disclaimer', level=log.INFO)
-        driver.find_element(By.NAME, 'cbAgree').click()
-        driver.find_element(By.ID, 'B1').click()
-        self.log('Loading search by Orange County', level=log.INFO)
-        driver.get(
-            'http://www.meganslaw.ca.gov/search_main.aspx?\
-            searchBy=county&county=orange&lang=ENGLISH')
-        time.sleep(2)
-        self.log('Loading list view', level=log.INFO)
-        driver.execute_script('ViewList("countylist")')
-        time.sleep(3)
-        self.log('Scraping session data:', level=log.INFO)
-        url = driver.current_url
-        self.pages = int(driver.find_element(
-            By.XPATH,
-            '((//td[@align="right" and @valign="top"])[1]/a)[last()]'
-            ).text.strip())
-        self.session_id = parse_qs(url)['W6'][0].strip()
-        self.log('SESSION ID:', self.session_id, level=log.INFO)
-        self.log('PAGE COUNT:', self.pages, level=log.INFO)
-        driver.quit()
-        self.log('Quitting Selenium', level=log.INFO)
-
     def parse(self, response):
-        # First page
-        self.login()
-        for x in xrange(1, self.pages + 1):
-            self.log('Requesting page: %s' % x, level=log.INFO)
-            yield Request(
-                self.pagination_url % (self.session_id, x),
-                callback=self.parse_page)
+        self.log('Fetching session data', level=log.INFO)
+        self.session_id = response.body.strip()
+        self.log('SESSION ID: %s' % self.session_id, level=log.INFO)
+        self.log('Requesting first page', level=log.INFO)
+        yield Request(
+            self.pagination_url % (self.session_id, 1),
+            callback=self.parse_first_page)
+
+        
